0aaa0d3c3dbfac3c833102fa1eef2ed331e1cd1c
Amber Oglesby <aoglesby5795@gmail.com>
2023-02-01 06:55:24 -0500

    hw attempt

--- src/App.js
+++ src/App.js
@@ -1,17 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
-import ImageForm from './components/form';
-import ImageList from './components/list';
+import logo from "./logo.svg";
+import "./App.css";
+import ImageForm from "./components/form";
+import ImageList from "./components/list";
+import { useState } from "react";
 
+const INITIAL_IMAGES = [
+  {
+    id: 1,
+    image: "https://unsplash.com/photos/t69fIZXxrwQ",
+    caption: "Terracotta Stairwell",
+  },
+  {
+    id: 2,
+    image: "https://unsplash.com/photos/W5dsm9n6e3g",
+    caption: "Earth tone interioir design",
+  },
+  { id: 3, image: "https://unsplash.com/photos/BlIhVfXbi9s", caption: "desk" },
+];
 
+function ImageBoard() {
+  const [images, setImages] = useState(INITIAL_IMAGES);
+  const imagesHTML = images.map((image) => (
+    <Image key={image.id} image={image} />
+  ));
 
-function ImageBoard{
-  return(
-    <div>
-      <ImageForm />
-      <ImageList />
+  return (
+    <div className="image-board-list">
+      <div class="card" style="width: 18rem;">
+        <img src={images} class="card-img-top" alt="..." />
+        <div class="card-body">
+          <p class={caption}></p>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
 export default ImageBoard;
--- src/components/form.js
+++ src/components/form.js
@@ -1,5 +1,31 @@
-function ImageForm() {
-  return <form>...</form>;
-}
+function ImageForm({ addImage }) {
+  const [name, setName] = useState("");
 
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const todo = {
+      id: nanoid(),
+      name,
+    };
+
+    addImage(image);
+    setName("");
+  };
+
+  const handleInput = (event) => {
+    setName(event.target.value);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div className="mb-3">
+        <label htmlFor="name" className="form-label">
+          Add new Image
+        </label>
+      </div>
+      <button type="submit">save</button>
+    </form>
+  );
+}
 export default ImageForm;
